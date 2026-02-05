82c083c8031bb64d7a79afea1a7b8c6459b3fd12
HARIHARA P <harigomathi20@gmail.com>
2026-02-05 18:00:21 +0530

    Implemented History Page UI

--- lawform/src/app/app.routes.ts
+++ lawform/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Layout } from './Components/layout/layout';
 import { Home } from './Pages/home/home';
+import { History } from './Pages/history/history';
 export const routes: Routes = [
     {
         path:'',
@@ -9,6 +10,10 @@ export const routes: Routes = [
             {
                 path:'home',
                 component:Home
+            },
+            {
+                path:'history',
+                component:History
             },
             {
                 path:'',
